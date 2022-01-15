0cb53f7194adb37abeef3dc8e26dbc72f197c12c
eleosmissionsusa <95595775+eleosmissionsusa@users.noreply.github.com>
2022-01-15 14:01:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("test");
+
+function search() {
+    console.log("testthing");
+    var input = document.getElementById("searchinput").value;
+    console.log(input)
+    return false;
+}
